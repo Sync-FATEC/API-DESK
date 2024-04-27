116bf7781b66fb13957dac00404ee6b7dc3a1d40
Claudio Jayme <142222453+ClaudioJaymeDiniz@users.noreply.github.com>
2024-04-27 11:59:28 -0300

    feat: Adicionando links no AsideAdmin e modificando css

--- front-end/src/components/AsideAdmin/index.tsx
+++ front-end/src/components/AsideAdmin/index.tsx
@@ -3,15 +3,16 @@ import './AsideAdmin.css'
 export const AsideAdmin = () =>{
     return(
             <div className="box_de_links">
-                    <h5 style={{ textAlign: 'left' }}>Técnico</h5>
-                    <h5 style={{ textAlign: 'left' }}>Clientes</h5>
-                    <h5 style={{ textAlign: 'left' }}>Categoria</h5>
-                    <h5 style={{ textAlign: 'left' }}>Tipos de problemas</h5>
-                    <h5 style={{ textAlign: 'left' }}>Equipamento</h5>
-                    <h5 style={{ textAlign: 'left' }}>Salas</h5>
-                    <h5 style={{ textAlign: 'left' }}>FAQ</h5>
-                    <h5 style={{ textAlign: 'left' }}>Base de conhecimento</h5>
-                    <h5 style={{ textAlign: 'left' }}>Templates de finalização</h5>
+                    <a href="/cadastrotecnico"><h5>Técnico</h5></a>
+                    <a href="/clientesadm"><h5>Clientes</h5> </a>
+                    <a href="/categoriaadm"><h5>Categoria</h5></a>
+                    <a href="/tiposproblemaadm"><h5>Tipos de problemas</h5></a>
+                    <a href="/equipamentoadm"><h5>Equipamento</h5></a>
+                    <a href="/salaadm"><h5>Salas</h5></a>
+                    <a href="/faqadm"><h5>FAQ</h5></a>
+                    <a href="/baseadm"><h5>Base de conhecimento</h5></a>
+                    <a href="/templateadm"><h5>Templates de finalização</h5></a>
+                    
                 </div>
     )
 }
