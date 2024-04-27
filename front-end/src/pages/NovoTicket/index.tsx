c1546b3da741003d54ff0ddf9264a3adc1a65af7
Ana-Laura-Moratelli <analauramoratelli203@gmail.com>
2024-04-27 17:42:18 -0300

    feat: criando interface da função de novo ticket

---
+++ front-end/src/pages/NovoTicket/index.tsx
@@ -0,0 +1,78 @@
+import './novoticket.css';
+import React, { useState } from 'react';
+
+export const NovoTicket = () => {
+
+    const [selectedImage, setSelectedImage] = useState<File | null>(null);
+
+    const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const input = event.target;
+        const file = input.files?.[0];
+        
+        if (file) {
+            console.log('Arquivo selecionado:', file.name);
+            setSelectedImage(file);
+        }
+    };
+
+    const handleCancelUpload = () => {
+        setSelectedImage(null);
+        const input = document.getElementById('image-upload') as HTMLInputElement;
+        if (input) {
+            input.value = ''; // Limpa o campo de entrada do arquivo
+        }
+    };
+
+    return (
+        <div className="modal-ticket">
+            
+            <h1>Novo Ticket</h1>
+            <hr />
+            <div className='Filter-tickets'>
+                
+                <select className="selectCategoria">
+                    <option value="abertos">Categoria</option>
+                    <option value="emAtendimento">Em Atendimento</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="finalizados">Finalizados</option>
+                </select>
+                <select className="selectCategoria">
+                    <option value="abertos">Tipo de problema</option>
+                    <option value="emAtendimento">Em Atendimento</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="finalizados">Finalizados</option>
+                </select>
+                <select className="selectCategoria">
+                    <option value="abertos">Equipamento</option>
+                    <option value="emAtendimento">Em Atendimento</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="finalizados">Finalizados</option>
+                </select>
+                <select className="selectCategoria">
+                    <option value="abertos">Sala</option>
+                    <option value="emAtendimento">Em Atendimento</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="finalizados">Finalizados</option>
+                </select>
+            </div>
+
+            <div className="formInput-assunto">
+              <input className='assunto' type="text" placeholder="Assunto do ticket"/>
+             
+            </div>
+            <div className="formInput-assunto">
+              <input className='assunto' type="text" placeholder="Descrição do ticket"/>
+            </div>
+            <div className="formInput-imagem">
+                <input id="image-upload" type="file" accept="image/*" onChange={handleImageUpload} />
+                {selectedImage && (
+                    
+                <button onClick={handleCancelUpload}>X</button>
+                   
+                )}
+            </div>
+            <button>Enviar Ticket</button>
+            </div>
+       
+    );
+};
