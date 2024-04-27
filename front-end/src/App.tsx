ac568a166a871ed7955c67c0655ac8d6a2292a3f
Claudio Jayme <142222453+ClaudioJaymeDiniz@users.noreply.github.com>
2024-04-27 11:56:01 -0300

    feat: Adicionando rotas das paginas de cadastro ADM

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -7,11 +7,30 @@ import { Admin } from './pages/Admin';
 import { Tecnico } from './pages/Tecnico';
 import { Error } from './pages/Error';
 import { RequireAuth } from './contexts/Auth/RequireAuth';
+import { CadastroTecnico } from './pages/Admin/CadastroTecnico/cadastroTecnico';
+import { ClientesAdm } from './pages/Admin/ClientesAdm/clientesAdm';
+import { CategoriaAdm } from './pages/Admin/CategoriaAdm/categoriaAdm';
+import { TiposProblemaAdm } from './pages/Admin/TiposProblemaAdm/tiposProblemaAdm';
+import { EquipamentoAdm } from './pages/Admin/EquipamentoAdm/equipamentoAdm';
+import { SalaAdm } from './pages/Admin/SalaAdm/salaAdm';
+import { BaseAdm } from './pages/Admin/BaseAdm/baseAdm';
+import { TemplateAdm } from './pages/Admin/TemplateAdm/templateAdm';
+import { FaqAdm } from './pages/Admin/FaqAdm/faqAdm';
+
 
 export const App =() =>{
   return (
     <div className="App">
         <Routes>
+          <Route path="/faqadm" element={<FaqAdm/>} />
+          <Route path="/templateadm" element={<TemplateAdm/>} />
+          <Route path="/baseadm" element={<BaseAdm/>} />
+          <Route path="/salaadm" element={<SalaAdm/>} />
+          <Route path="/equipamentoadm" element={<EquipamentoAdm/>} />
+          <Route path="/tiposproblemaadm" element={<TiposProblemaAdm/>} />
+          <Route path="/categoriaadm" element={<CategoriaAdm/>} />
+          <Route path="/clientesadm" element={<ClientesAdm/>} />
+          <Route path="/cadastrotecnico" element={<CadastroTecnico/>} />
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
