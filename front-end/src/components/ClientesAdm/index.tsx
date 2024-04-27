9b7cd5d4fac4feb9d671cd6b9c978a7da9d15f7e
maiconsso <mssmaiconsouza@gmail.com>
2024-04-27 17:17:49 -0300

    feat: alterando página ClientsAdmin e criando função para renderizar componentes de páginas em Admin

---
+++ front-end/src/components/ClientesAdm/index.tsx
@@ -0,0 +1,36 @@
+import './clientesAdm.css';
+
+const ClientesAdm = () => {
+    const clientes = [
+        { nome: 'Cliente 1', email: 'cliente1@example.com' },
+        { nome: 'Cliente 2', email: 'cliente2@example.com' },
+        { nome: 'Cliente 3', email: 'cliente3@example.com' }
+    ];
+
+    const handleDeleteUser = () => {
+        // BackEnd logica para excluir e logica para pegar os emails
+    };
+
+    return (
+        <div className="clientesContainer">
+            <div className="titulo">
+                <div className="formTitle">
+                    Clientes cadastrados
+                </div>
+            </div>
+
+            <div className="container">
+                {clientes.map((cliente, index) => (
+                    <div className="clienteEmail" key={index}>
+                            <p>{cliente.email}</p>
+                        <button className="excluir" onClick={handleDeleteUser}>
+                            Excluir
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ClientesAdm;
