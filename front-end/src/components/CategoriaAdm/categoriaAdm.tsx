86792c8753323de8ad16a91d176bfd3f22302809
João Gabriel <joaoggbs62@gmail.com>
2024-04-28 12:48:35 -0300

    Feat: Feita a integração do sistema de criar e excluir categorias

--- front-end/src/components/CategoriaAdm/categoriaAdm.tsx
+++ front-end/src/components/CategoriaAdm/categoriaAdm.tsx
@@ -1,21 +1,99 @@
-import './categoriaAdm.css';
 
-export const CategoriaAdm = () => {
+import './categoriaAdm.css';
+import add from '../../assets/img/add.png';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import ICategoria from '../../types/ICategoria';
+
+const CategoriaAdm = () => {
+    const [categorias, setCategorias] = useState<ICategoria[]>([]);
+    const [categoria, setCategoria] = useState('');
+    const [tipoTecnico, setTipoTecnico] = useState('');
+
+    const handleCategoriaChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCategoria(event.target.value);
+    };
+
+    const handleTipoTecnicoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTipoTecnico(event.target.value);
+    };
+
+    useEffect(() => {
+        const fetchSalas = async () => {
+          try {
+            const response = await axios.get('http://localhost:5555/categorias/listar');
+            setCategorias(response.data);
+          } catch (error) {
+            console.error(error);
+          }
+        };
+    
+        fetchSalas();
+      }, []);
+
+    const handleDeleteUser = (categoriaID: number) => {
+        try {
+            const response = axios.delete('http://localhost:5555/categorias/excluir', {
+                params: {
+                    categoriaID: categoriaID
+                }
+            });
+            setCategorias(categorias.filter((categoria) => categoria.categoriaID !== categoriaID));
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    const handleAddUser = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        
+        if (!categoria || !tipoTecnico) {
+            alert('Preencha todos os campos');
+            return;
+        }
+        try {
+            const response = await axios.post('http://localhost:5555/categorias/cadastrar', {
+                categoria: categoria,
+                tipoTecnico: tipoTecnico
+            });
+            setCategorias([...categorias, response.data]);
+        } catch (error) {
+            console.error(error);
+        }
+    };
     return (
-        <div>
+        <div className="adminContainer">
             <div className="titulo">
-                <div className="texto_titulo">
-                    <h3>Configurações de cadastro categoria de ticket</h3>
-                </div>
             </div>
 
             <div className="container">
-                <div className="box_de_conteudo">
-                    <h4>teste</h4>
-                    <h4>teste</h4>
-                    <h4>teste</h4>
-                    
+                {categorias.map((categorias, index) => (
+                    <div className="numeroSala" key={categorias.categoriaID}>
+                            <p>{categorias.categoria}</p>
+                            <p>{categorias.tipoTecnico}</p>
+                        <button className="excluir" onClick={() => handleDeleteUser(categorias.categoriaID)}>
+                            Excluir
+                        </button>
+                    </div>
+                ))}
+                <div className="numeroSala">
+                    <form onSubmit={handleAddUser} method='post'>
+                        <input onChange={handleCategoriaChange} type="text" className="inputSala" placeholder="Adicionar categoria"/>
+                        <div className="formInput">
+                            <label className="labelInputTecnico" htmlFor="categoria">Categoria do técnico</label>
+                            <select className="selectCategoria" value={tipoTecnico} onChange={(e) => setTipoTecnico(e.target.value)}>
+                                <option value=""></option>
+                                <option value="1">Suport N1</option>
+                                <option value="2">Suporte N2</option>
+                                <option value="3">Suporte N3</option>
+                            </select>
+                        </div>
+                        <button type='submit'>Criar nova categoria</button>
+                    </form>
+                <img src={add} alt="add" />
                 </div>
+                
             </div>
         </div>
     )};
+
+export default CategoriaAdm;
