a4d7c84de0649a1cb5c2f6341012a423ce84e350
João Gabriel <joaoggbs62@gmail.com>
2024-04-28 05:37:53 -0300

    feat: Integração do sistema de criar e excluir salas

--- front-end/src/components/SalaAdm/index.tsx
+++ front-end/src/components/SalaAdm/index.tsx
@@ -1,19 +1,65 @@
 
 import './salaAdm.css';
 import add from '../../assets/img/add.png';
+import axios from 'axios';
+import Sala from '../../types/Sala';
+import { useEffect, useState } from 'react';
 
 const SalaAdm = () => {
-    const sala = [
-        { nome: 'Sala 401'},
-        { nome: 'Sala 402' },
-        { nome: 'Sala 403'}
-    ];
+    const [sala, setSalas] = useState<Sala[]>([]);
+    const [numeroSala, setNumeroSala] = useState('');
+    const [identificacao, setIdentificacao] = useState('');
 
-    const handleDeleteUser = () => {
-        // BackEnd logica para excluir e logica para pegar as salas
+    const handleNumeroSalaChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNumeroSala(event.target.value);
     };
-    const handleAddUser = () => {
-        // BackEnd logica para adicionar
+
+    const handleIdentificacaoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setIdentificacao(event.target.value);
+    };
+
+    useEffect(() => {
+        const fetchSalas = async () => {
+          try {
+            const response = await axios.get('http://localhost:5555/salas/visualizar');
+            setSalas(response.data);
+          } catch (error) {
+            console.error(error);
+          }
+        };
+    
+        fetchSalas();
+      }, []);
+
+    const handleDeleteUser = (numeroSala: number) => {
+        try {
+            const response = axios.delete('http://localhost:5555/salas/excluir', {
+                params: {
+                    numeroSala: numeroSala
+                }
+            });
+            setSalas(prevSalas => prevSalas.filter(s => s.numeroSala !== numeroSala));
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    const handleAddUser = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        
+        if (!numeroSala || !identificacao) {
+            alert('Preencha todos os campos');
+            return;
+        }
+
+        try {
+            const response = await axios.post('http://localhost:5555/salas/criar', {
+                numeroSala: numeroSala,
+                identificacao: identificacao
+            });
+            setSalas(prevSalas => [...prevSalas, response.data]);
+        } catch (error) {
+            console.error(error);
+        }
     };
     return (
         <div className="adminContainer">
@@ -23,19 +69,20 @@ const SalaAdm = () => {
             <div className="container">
                 {sala.map((sala, index) => (
                     <div className="numeroSala" key={index}>
-                            <p>{sala.nome}</p>
-                        <button className="excluir" onClick={handleDeleteUser}>
+                            <p>{sala.numeroSala}</p>
+                            <p>{sala.identificacao}</p>
+                        <button className="excluir" onClick={() => handleDeleteUser(sala.numeroSala)}>
                             Excluir
                         </button>
                     </div>
-                    
-                    
                 ))}
                 <div className="numeroSala">
-                    <input type="text" className="inputSala" placeholder="Adicionar Sala"/>
-                    <button onClick={handleAddUser} className="add">
+                    <form onSubmit={handleAddUser} method='post'>
+                        <input onChange={handleNumeroSalaChange} type="number" className="inputSala" placeholder="Adicionar Sala"/>
+                        <input onChange={handleIdentificacaoChange} type="text" className='inputSala' placeholder='Adicionar identificação'/>
+                        <button type='submit'>Criar nova sala</button>
+                    </form>
                 <img src={add} alt="add" />
-            </button>
                 </div>
                 
             </div>
---
+++ front-end/src/types/Sala.ts
@@ -0,0 +1,5 @@
+export default interface Sala {
+    salaId: number;
+    numeroSala: number;
+    identificacao: string;
+}
