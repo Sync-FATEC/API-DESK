4c5d224670580229d1cdfa4d478d207ad665d9b6
João Gabriel <joaoggbs62@gmail.com>
2024-04-30 15:35:21 -0300

    Refactor: [bd] Adicionando Not Null nas tabelas e criando inserções para testes

--- back-end/bd/insert.sql
+++ back-end/bd/insert.sql
@@ -1,42 +1,24 @@
--- Inserir alguns usuários
-INSERT INTO Usuarios (nome, cpf, email, senha, tipoUsuario, turno)
-VALUES
-('João', '12345678901', 'joao@example.com', 'senha123', 'A', 'M'),
-('Maria', '98765432101', 'maria@example.com', 'senha456', '1', 'T'),
-('Pedro', '55544433322', 'pedro@example.com', 'senha789', 'U', NULL);
- 
--- Inserir algumas categorias
+-- Inserts para a tabela Categorias
 INSERT INTO Categorias (categoria, tipoTecnico)
-VALUES
-('Hardware', '1'),
-('Software', '2'),
-('Redes', '3');
- 
--- Inserir algumas salas
-INSERT INTO Salas (numeroSala, indentificacao)
-VALUES
-(101, 'Sala de Reuniões'),
-(102, 'Sala de Treinamento'),
-(103, 'Sala de Servidores');
- 
--- Inserir alguns equipamentos
-INSERT INTO Equipamentos (tipoEquipamento, equipamento, sla, prioridade, salaID, categoriaID)
-VALUES
-('Notebook', 'Dell Latitude', NOW(), 1, 1, 1),
-('Projetor', 'BenQ MX550', NOW(), 2, 2, 1),
-('Switch', 'Cisco Catalyst 2960', NOW(), 3, 3, 3);
- 
--- Inserir algumas mensagens
-INSERT INTO Mensagens (tipoMensagem, titulo, mensagem, categoriaID)
-VALUES
-('1', 'Aviso Importante', 'Manutenção programada para hoje às 20:00', 2),
-('2', 'Atualização de Software', 'Será realizada uma atualização de software amanhã às 3:00', 2),
-('3', 'Problema de Rede', 'O servidor de email está fora do ar', 3);
- 
--- Inserir alguns tickets
--- Inserir alguns tickets
-INSERT INTO Tickets (dataAbertura, dataFechamento, titulo, descricao, status, categoriaID, equipamentosID, salaID, usuarioID, tipoTecnico)
-VALUES
-('2024-04-20 10:00:00', NULL, 'Problema de Conexão', 'Usuário relatou problemas de conexão à internet.', 'A', 3, 1, 1, 1, 'A'),
-('2024-04-21 09:30:00', NULL, 'Problema com Impressora', 'Impressora não está imprimindo.', 'A', 1, 2, 2, 2, 'A'),
-('2024-04-21 11:15:00', NULL, 'Solicitação de Licença', 'Usuário solicita licença para software específico.', 'A', 2, 3, NULL, NULL, 'A');
+VALUES ('Hardware', '1'),
+       ('Software', '2'),
+       ('Redes', '3');
+
+-- Inserts para a tabela salas
+INSERT INTO salas (numeroSala, indentificacao)
+VALUES (101, 'Sala de Reuniões'),
+       (102, 'Laboratório de Informática'),
+       (103, 'Sala de Servidores');
+
+-- Inserts para a tabela equipamentos
+INSERT INTO equipamentos (equipamento, sla, prioridade, salaID, categoriaID)
+VALUES ('Computador', 3, 'Alta', 1, 1),
+       ('Roteador', 2, 'Média', 2, 3),
+       ('Impressora', 4, 'Baixa', 3, 1);
+
+-- Inserts para a tabela mensagens
+INSERT INTO mensagens (tipoMensagem, titulo, mensagem, categoriaID)
+VALUES ('F', 'Manutenção Programada', 'Haverá uma manutenção na rede na próxima semana.', 3),
+       ('B', 'Atualização de Software', 'Será realizada uma atualização nos sistemas na sexta-feira.', 2),
+       ('F', 'Manutenção Programada', 'Haverá uma manutenção na rede na próxima semana.', 3),
+       ('T', 'Manutenção Programada', 'Haverá uma manutenção na rede na próxima semana.', 3);
--- back-end/bd/tabelas.sql
+++ back-end/bd/tabelas.sql
@@ -3,62 +3,61 @@ use api;
 
 create table Usuarios (
 	usuarioID int primary key auto_increment,
-    nome varchar(255),
-    cpf varchar(11) unique,
-    email varchar(255) unique,
-    senha varchar(255),
+    nome varchar(255) not null,
+    cpf varchar(11) unique not null,
+    email varchar(255) unique not null,
+    senha varchar(255) not null,
     tipoUsuario varchar(1) default 1,
     turno varchar(5) default NULL
 );
 
 create table Categorias (
 	categoriaID int primary key auto_increment,
-    categoria varchar(255),
-    tipoTecnico varchar(1)
+    categoria varchar(255) not null,
+    tipoTecnico varchar(1) not null
 );
 
 create table salas (
 	salaID int primary key auto_increment,
-    numeroSala int unique,
+    numeroSala int unique not null,
     indentificacao varchar(255)
 );
 
 create table equipamentos (
 	equipamentosID int primary key auto_increment,
-    equipamento varchar(255),
-    sla int,
-    prioridade varchar(50),
-    salaID int,
-    categoriaID int,
+    equipamento varchar(255) not null,
+    sla int not null,
+    prioridade varchar(50) not null,
+    salaID int not null,
+    categoriaID int not null,
     foreign key (salaID) references salas(salaID),
     foreign key (categoriaID) references Categorias(categoriaID)
 );
 
 create table mensagens (
 	mensagemID int primary key auto_increment,
-    tipoMensagem varchar(1),
-    titulo varchar(255),
+    tipoMensagem varchar(1) not null,
+    titulo varchar(255) not null,
     mensagem varchar(255),
-    categoriaID int,
+    categoriaID int not null,
     foreign key (categoriaID) references categorias(categoriaID)
 );
 
 create table tickets (
 	ticketsID int primary key auto_increment,
-    dataAbertura datetime,
+    dataAbertura datetime not null,
     dataFechamento datetime,
-    titulo varchar(255),
-    descricao varchar(255),
-    status varchar(1),
-    prioridade int,
-    categoriaID int,
-    equipamentosID int,
-    salaID int,
-    usuarioID int,
-    tipoTecnico varchar(1),
+    titulo varchar(255) not null,
+    descricao varchar(255) not null,
+    status varchar(1) default '1',
+    prioridade int not null,
+    categoriaID int not null,
+    equipamentosID int not null,
+    salaID int not null,
+    usuarioID int not null,
+    tipoTecnico varchar(1) not null,
     foreign key (categoriaID) references categorias(categoriaID),
     foreign key (equipamentosID) references equipamentos(equipamentosID),
     foreign key (salaID) references salas(salaID),
     foreign key (usuarioID) references usuarios(usuarioID)
 );
-    
