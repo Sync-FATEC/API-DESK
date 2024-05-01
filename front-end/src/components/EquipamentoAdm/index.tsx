3d268f1efb17c9f8cd28e497416ae691fbf57c02
Filipe Colla <filipe10colla@gmail.com>
2024-05-01 00:13:22 -0300

    Refactor: ( Back ) Avisos de cadastramento de equipamentos e excluimento aprimorados

--- front-end/src/components/EquipamentoAdm/index.tsx
+++ front-end/src/components/EquipamentoAdm/index.tsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import ICategoria from '../../types/ICategoria';
 import IEquipamentos from '../../types/IEquipamentos';
 import ISalas from '../../types/ISalas';
+import { warning, erro, Toast } from '../Swal/swal';
 
 const EquipamentoAdm = () => {
     const [equipamento, setEquipamento] = useState('')
@@ -20,7 +21,7 @@ const EquipamentoAdm = () => {
           try {
             const categoria = await axios.get('http://localhost:5555/categorias/listar');
             setCategorias(categoria.data);
-            const sala = await axios.get('http://localhost:5555/salas/visualizar');            
+            const sala = await axios.get('http://localhost:5555/salas/visualizar');
             setSalas(sala.data);
             const equip = await axios.get('http://localhost:5555/equipamentos/listar');
             setEquipamentos(equip.data);
@@ -37,7 +38,7 @@ const EquipamentoAdm = () => {
         event.preventDefault();
         
         if (!equipamento|| !categoriaID || !numeroSala || !sla || !prioridade) {
-            alert('Preencha todos os campos');
+            warning('Preencha todos os campos');
             return;
         }
         try {
@@ -47,24 +48,71 @@ const EquipamentoAdm = () => {
                 prioridade: prioridade,
                 categoriaID: categoriaID,
                 numeroSala: numeroSala
-
             });
             setEquipamentos([...equipamentos, response.data]);
+            Toast.fire({
+            icon: "success",
+            title: "Criado com sucesso!"
+            })
+            setEquipamento('');
+            setNumeroSala('');
+            setCategoriaID('');
+            setPrioridade('');
+            setSla('');
+
         } catch (error) {
             console.error(error);
+            erro('Error!');
+            setEquipamento('');
+            setNumeroSala('');
+            setCategoriaID('');
+            setPrioridade('');
+            setSla('');
         }
-    };
+
+    }
+
+    const handleEquipamento = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newEquipamento = e.target.value;
+        setEquipamento(newEquipamento);
+    }
+
+    const handleNumeroSala = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const newNumeroSala = e.target.value;
+        setNumeroSala(newNumeroSala);
+    }
+
+    const handleCategoriaID = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const newCategoriaID = e.target.value;
+        setCategoriaID(newCategoriaID);
+    }
+
+    const handlePrioridade = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const newPrioridade = e.target.value;
+        setPrioridade(newPrioridade);
+    }
+
+    const handleSla = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newSla = e.target.value;
+        setSla(newSla);
+    }
 
     const handleDeleteUser = async (equipamentosID: number) => {
         try {
             const response = await axios.delete('http://localhost:5555/equipamentos/excluir/' + equipamentosID);
             if (response.data === 'Ocorreu um erro para excluir o equipamento') {
-                alert('Ocorreu um erro para excluir o equipamento');
+                erro('Ocorreu um erro para excluir o equipamento');
                 return;
             }
             setEquipamentos(equipamentos.filter((equipamento) => equipamento.equipamentosID !== equipamentosID));
+            Toast.fire({
+            icon: "success",
+            title: "Excluido com sucesso!"
+            })
+
         } catch (error) {
             console.error(error);
+            erro('Error!')
         }
     };
 
@@ -94,8 +142,8 @@ const EquipamentoAdm = () => {
             ))}
             <div className="container">
                 <form onSubmit={handleAddUser} method="post">
-                    <input type="text" onChange={(e) => setEquipamento(e.target.value)} />
-                    <select onChange={(e) => setNumeroSala(e.target.value)}>
+                    <input type="text" onChange={handleEquipamento} value={equipamento}/>
+                    <select onChange={handleNumeroSala} value={numeroSala}>
                         <option value={undefined}>Selecione algo</option>
                         {salas.map((sala, index) => (
                             <option value={sala.numeroSala} key={index}>
@@ -103,7 +151,7 @@ const EquipamentoAdm = () => {
                             </option>
                         ))}
                     </select>
-                    <select onChange={(e) => setCategoriaID(e.target.value)}>
+                    <select onChange={handleCategoriaID} value={categoriaID}>
                         <option value={undefined}>Selecione algo</option>
                         {categorias.map((categoria, index) => (
                             <option value={categoria.categoriaID} key={index}>
@@ -111,13 +159,13 @@ const EquipamentoAdm = () => {
                             </option>
                         ))}
                     </select>
-                    <select onChange={(e) => setPrioridade(e.target.value)}>
+                    <select onChange={handlePrioridade} value={prioridade}>
                         <option value={undefined}>Selecione algo</option>
                         <option value="Baixa">Baixa</option>
                         <option value="Media">Média</option>
                         <option value="Alta">Alta</option>
                     </select>
-                    <input type="number" onChange={(e) => setSla(e.target.value)} />
+                    <input type="number" onChange={handleSla} value={sla} />
                     <button type="submit">ENVIAR!</button>
                 </form>
             </div>
