d198595370b4765bcc183044ba8122c71308ba80
Filipe Colla <filipe10colla@gmail.com>
2024-05-02 20:24:46 -0300

    Fix: [ Back ] Listagem de ticket arrumada, listando apenas um invés de todos

--- front-end/src/components/VisualizarTicket/index.tsx
+++ front-end/src/components/VisualizarTicket/index.tsx
@@ -1,78 +1,55 @@
-import './visualizarticket.css';
-import { useContext, useEffect, useState } from 'react';
-import axios from 'axios';
-import ITickets from '../../types/ITickets';
+import React from 'react';
+import { useContext } from 'react';
 import { AuthContext } from '../../contexts/Auth/AuthContext';
+import './visualizarticket.css';
+import ITickets from '../../types/ITickets';
 
+interface Props {
+    selectedTicket: ITickets | null;
+    onClose: () => void;
+}
 
-export const VisualizarTicket = () => {
-  const [tickets, setTickets] = useState<ITickets[]>([]);
-  const user = useContext(AuthContext);
+const VisualizarTicket: React.FC<Props> = ({ selectedTicket, onClose }) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const user = useContext(AuthContext);
 
-  useEffect(() => {
-    const fetchSalas = async () => {
-      try {
-        const response = await axios.get('http://localhost:5555/tickets/listar/' + user.user?.usuarioID);
+    // Verifica se o ticket está selecionado
+    if (!selectedTicket) return null;
 
+    return (
+        <div className="modalVisualizar">
+            <div className='btnVisualizar'> 
+                <div>
+                    <span className="material-symbols-outlined">sync_alt</span>
+                    <span id='btnAtendendo' className="material-symbols-outlined">play_circle</span>
+                    <span id='btnPendente' className="material-symbols-outlined">pause_circle</span>
+                    <span id='btnFinalizar' className="material-symbols-outlined">check_circle</span>
+                </div>
+                <div>
+                    <span id='btnSla' className="material-symbols-outlined">bomb</span>
+                </div>
+            </div>
 
-        setTickets(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+            <div className='infoContainer'>
+                <div className="infoVisualizar">
+                    <h2>{selectedTicket.titulo}</h2>
+                    <p>{selectedTicket.descricao}</p>
+                    <p>{selectedTicket.status}</p>
+                    <p>{selectedTicket.categoria.categoria}</p>
+                    <p>{selectedTicket.equipamentos.equipamento}</p>
+                    <p>{selectedTicket.sala.numeroSala}</p>
+                    <p>{selectedTicket.usuario.nome}</p>
+                    <div>{new Date(selectedTicket.dataAbertura).toLocaleDateString('pt-BR')}</div>
+                </div>
+                <div className='infoChat'>
+                    {/* Aqui você pode adicionar o componente de chat, se necessário */}
+                </div>
+            </div>
 
-    fetchSalas();
-  }, [user.user]);
-
-  return (
-    <div className="modalVisualizar">
-
-      <div className='btnVisualizar'> 
-      <div>
-
-      <span className="material-symbols-outlined">
-        sync_alt
-      </span>
-        <span id='btnAtendendo' className="material-symbols-outlined">
-          play_circle
-        </span>
-        <span id='btnPendente' className="material-symbols-outlined">
-          pause_circle
-        </span>
-        <span id='btnFinalizar' className="material-symbols-outlined">
-          check_circle
-        </span>
+            {/* Botão para fechar o modal */}
+            <span className="close" onClick={onClose}>&times;</span>
         </div>
-        <div>
-          
-        <span id='btnSla' className="material-symbols-outlined">
-          bomb
-        </span>
-        </div>
-      </div>
-
-      <div className='infoContainer'>
-
-
-        {tickets.map((tickets, index) => (
-          <div className="infoVisualizar" key={index}>
-            <h2>{tickets.titulo}</h2>
-            <p>{tickets.descricao}</p>
-            <p>{tickets.status}</p>
-            <p>{tickets.categoria.categoria}</p>
-            <p>{tickets.equipamentos.equipamento}</p>
-            <p>{tickets.sala.numeroSala}</p>
-            <p>{tickets.usuario.nome}</p>
-            <div>{new Date(tickets.dataAbertura).toLocaleDateString('pt-BR')}</div>
-          </div>
-        ))}
-
-        <div className='infoChat'>
-
-        </div>
-      </div>
-    </div>
-
-
-  );
+    );
 };
+
+export default VisualizarTicket;
