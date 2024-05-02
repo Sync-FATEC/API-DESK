bd595f6f8b27f213f3648d537e0050ef9052939d
Filipe Colla <filipe10colla@gmail.com>
2024-05-02 20:31:18 -0300

    Fix: [ Front ] Adicionado os elementos que seram usados na listagem de tickets, para poder usar no front

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,13 @@
 import './global.css';
 import { Route, Routes } from 'react-router-dom';
-import { Cliente } from './pages/Cliente';
+import  Cliente  from './pages/Cliente';
 import { Login } from './pages/Login';
 import { Cadastro } from './pages/Cadastro';
 import { Admin } from './pages/Admin';
 import { Tecnicos } from './pages/Tecnico';
 import { RequireAuth } from './contexts/Auth/RequireAuth';
 import { FAQ } from './components/Faq';
-import { VisualizarTickets } from './pages/VisualizarTickets';
+import VisualizarTickets  from './pages/VisualizarTickets';
 
 export const App = () => {
   return (
@@ -18,7 +18,9 @@ export const App = () => {
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/FAQ" element={<RequireAuth tipoUsuario={['1', '2', '3', 'U', 'A']}><FAQ /></RequireAuth>} />
         <Route path="/admin" element={<RequireAuth tipoUsuario="A"><Admin /></RequireAuth>} />
-        <Route path="/visualizarTickets" element={<RequireAuth tipoUsuario="A"><VisualizarTickets /></RequireAuth>} />
+        <Route path="/visualizarTickets" element={<RequireAuth tipoUsuario="U"><VisualizarTickets selectedTicket={null} onClose={function (): void {
+          throw new Error('Function not implemented.');
+        } } /></RequireAuth>} />
         <Route path="/cliente" element={<RequireAuth tipoUsuario="U"><Cliente /></RequireAuth>} />
         <Route path="/tecnico" element={<RequireAuth tipoUsuario={['1', '2', '3']}><Tecnicos /></RequireAuth>} />
       </Routes>
