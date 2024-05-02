1cb23a747471d09132bf2cf208a620fe24c3916c
Filipe Colla <filipe10colla@gmail.com>
2024-05-02 20:27:53 -0300

    Fix: [ Front ] Bug no listamento de ticket na parte do front do técnico, arrumado

--- front-end/src/pages/Tecnico/index.tsx
+++ front-end/src/pages/Tecnico/index.tsx
@@ -3,24 +3,25 @@ import { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import ITickets from '../../types/ITickets';
 import { AuthContext } from '../../contexts/Auth/AuthContext';
-import { VisualizarTicket } from '../../components/VisualizarTicket';
-import './tecnico.css'
+import VisualizarTicket from '../../components/VisualizarTicket'; // Importe o componente VisualizarTicket
+import './tecnico.css';
+
 export const Tecnicos = () => {
-
   const [isVisualizarTicketModalOpen, setIsVisualizarTicketModalOpen] = useState(false);
-  const handleOpenVisualizarTicketModal = () => setIsVisualizarTicketModalOpen(true);
+  const [selectedTicket, setSelectedTicket] = useState<ITickets | null>(null); // Estado para armazenar o ticket selecionado
+
+  const handleOpenVisualizarTicketModal = (ticket: ITickets) => {
+    setSelectedTicket(ticket);
+    setIsVisualizarTicketModalOpen(true);
+  };
+
   const handleCloseVisualizarTicketModal = () => setIsVisualizarTicketModalOpen(false);
-
-
-
-
-
 
   const [tickets, setTickets] = useState<ITickets[]>([]);
   const user = useContext(AuthContext);
 
   useEffect(() => {
-    const fetchSalas = async () => {
+    const fetchTickets = async () => {
       try {
         const response = await axios.get('http://localhost:5555/tickets/listar/' + user.user?.usuarioID);
         setTickets(response.data);
@@ -29,18 +30,14 @@ export const Tecnicos = () => {
       }
     };
 
-    fetchSalas();
+    fetchTickets();
   }, [user.user]);
 
   return (
     <div className="ticketContainer">
       <div className="formTicket">
         <Header />
-
         <div className="ticket-container">
-
-
-
           <h1>Listagem de Tickets</h1>
           <div className="ticket-filters">
             <div className="filter-containerTec">
@@ -50,9 +47,6 @@ export const Tecnicos = () => {
                 <option value="pendentes">Pendentes</option>
                 <option value="finalizados">Finalizados</option>
               </select>
-
-
-
               <select className="selectFilter">
                 <option value="abertos">Categoria</option>
                 <option value="emAtendimento">Em Atendimento</option>
@@ -60,13 +54,10 @@ export const Tecnicos = () => {
                 <option value="finalizados">Finalizados</option>
               </select>
             </div>
-
-
           </div>
         </div>
         <div className='ticket-contentTec'>
           <div className="ticket-listTec">
-
             <div className="ticket-typesTec">
               <div className='afazer'>A fazer
                 <span className="material-symbols-outlined">
@@ -76,11 +67,9 @@ export const Tecnicos = () => {
               <div className='atendendo'>Atendendo</div>
               <div className='pendente'>Pendente</div>
               <div className='finalizado'>Finalizado</div>
-
             </div>
             {tickets.map((ticket, index) => (
               <div key={index} className="ticket-itemTec">
-
                 <div className='afazerInfo'>
                   <div>
                     #{ticket.ticketsID}
@@ -92,28 +81,24 @@ export const Tecnicos = () => {
                     <span className='prioridade'>Médio</span>
                     {new Date(ticket.dataAbertura).toLocaleDateString('pt-BR')}
                   </div>
-
-
                 </div>
                 <div className='atendendo'>Atendendo</div>
                 <div className='pendente'>Pendente</div>
-                <div className='finalizado' onClick={handleOpenVisualizarTicketModal}>Finalizado</div>
-              </div> 
+                <div className='finalizado' onClick={() => handleOpenVisualizarTicketModal(ticket)}>Finalizado</div>
+              </div>
             ))}
-                {isVisualizarTicketModalOpen && (
-                  <div className="modal">
-                    <div className="modal-content">
-                      <span className="close" onClick={handleCloseVisualizarTicketModal}>&times;</span>
-                      <VisualizarTicket />
-                    </div>
-                  </div>
-                )}
-              
-
-           
           </div>
         </div>
+        {/* Renderiza o modal VisualizarTicket apenas se o modal estiver aberto e houver um ticket selecionado */}
+        {isVisualizarTicketModalOpen && selectedTicket && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={handleCloseVisualizarTicketModal}>&times;</span>
+              <VisualizarTicket selectedTicket={selectedTicket} onClose={handleCloseVisualizarTicketModal} />
+            </div>
+          </div>
+        )}
       </div>
-    </div >
+    </div>
   );
 };
