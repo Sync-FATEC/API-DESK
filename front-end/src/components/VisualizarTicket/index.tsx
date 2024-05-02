6dd60c172329fdf84a06084fe358114850dd331b
Ana-Laura-Moratelli <analauramoratelli203@gmail.com>
2024-05-02 12:11:26 -0300

    style: [front] estilizando botões de visualizar ticket

--- front-end/src/components/VisualizarTicket/index.tsx
+++ front-end/src/components/VisualizarTicket/index.tsx
@@ -1,29 +1,78 @@
 import './visualizarticket.css';
+import { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import ITickets from '../../types/ITickets';
+import { AuthContext } from '../../contexts/Auth/AuthContext';
 
 
 export const VisualizarTicket = () => {
- 
+  const [tickets, setTickets] = useState<ITickets[]>([]);
+  const user = useContext(AuthContext);
 
-    return (
-      <div className="modalVisualizar">
-            <h1>Titulo do ticket</h1>
-            <div className='infoContainer'>
+  useEffect(() => {
+    const fetchSalas = async () => {
+      try {
+        const response = await axios.get('http://localhost:5555/tickets/listar/' + user.user?.usuarioID);
 
-            
-            <div className='infoVisualizar'>
-               <p>ID: </p>
-               <p>Data de abertura: </p>
-               <p>Tipo de problema: </p>
-               <p>Equipamento: </p>
-               <p>Sala: </p>
-               <p>Descrição do problema: </p>
-            </div>
-            <div className='infoChat'>
 
-            </div>
-          </div>
+        setTickets(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchSalas();
+  }, [user.user]);
+
+  return (
+    <div className="modalVisualizar">
+
+      <div className='btnVisualizar'> 
+      <div>
+
+      <span className="material-symbols-outlined">
+        sync_alt
+      </span>
+        <span id='btnAtendendo' className="material-symbols-outlined">
+          play_circle
+        </span>
+        <span id='btnPendente' className="material-symbols-outlined">
+          pause_circle
+        </span>
+        <span id='btnFinalizar' className="material-symbols-outlined">
+          check_circle
+        </span>
+        </div>
+        <div>
+          
+        <span id='btnSla' className="material-symbols-outlined">
+          bomb
+        </span>
+        </div>
       </div>
 
-        
-    );
+      <div className='infoContainer'>
+
+
+        {tickets.map((tickets, index) => (
+          <div className="infoVisualizar" key={index}>
+            <h2>{tickets.titulo}</h2>
+            <p>{tickets.descricao}</p>
+            <p>{tickets.status}</p>
+            <p>{tickets.categoria.categoria}</p>
+            <p>{tickets.equipamentos.equipamento}</p>
+            <p>{tickets.sala.numeroSala}</p>
+            <p>{tickets.usuario.nome}</p>
+            <div>{new Date(tickets.dataAbertura).toLocaleDateString('pt-BR')}</div>
+          </div>
+        ))}
+
+        <div className='infoChat'>
+
+        </div>
+      </div>
+    </div>
+
+
+  );
 };
