5e0b68ae73296257a2cb5843cc3172cc96627c1e
maiconsso <mssmaiconsouza@gmail.com>
2024-05-03 18:26:36 -0300

    style:[front]melhorando o css da página tecnicosADM

--- front-end/src/components/TecnicoAdm/index.tsx
+++ front-end/src/components/TecnicoAdm/index.tsx
@@ -4,10 +4,10 @@ import './tecnicoAdm.css';
 import { erro, Toast } from '../Swal/swal';
 import ITecnicos from '../../types/ITecnicos';
 
- 
+
 const TecnicosAdm = () => {
     const [tecnicos, setTecnicos] = useState<ITecnicos[]>([]);
- 
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -17,17 +17,17 @@ const TecnicosAdm = () => {
                 console.error('Erro ao buscar técnicos:', error);
             }
         };
- 
+
         fetchData();
     }, []);
- 
+
     const handleDeleteUser = async (usuarioID: string) => {
         try {
             const response = await axios.delete('http://localhost:5555/usuarios/excluir/' + usuarioID);
             setTecnicos(tecnicos.filter((tecnico) => tecnico.usuarioID !== usuarioID));
             Toast.fire({
-            icon: "success",
-            title: "Excluido com sucesso!"
+                icon: "success",
+                title: "Excluido com sucesso!"
             })
 
         } catch (error) {
@@ -35,38 +35,36 @@ const TecnicosAdm = () => {
             erro('Erro ao excluir técnico');
         }
     };
- 
+
     return (
         <div className='adminContainer'>
-            <div className="container">
-                <h2>Técnicos:</h2>
-                {tecnicos.map((tecnico, index) => (
-                    <div className="clienteEmail" key={index}>
-                        <div className="linha">
-                        <div className="linhaEsquerda">
-                        {tecnico.nome} - {tecnico.email} </div>
-                        <div className="linhaDireita">
-                            <div className="itemLinha">
-                            N{tecnico.tipoUsuario}
-                            </div>
-                            <div className="itemLinha linhaTurno">
-                            <span className="material-symbols-outlined">alarm_add</span>
-                            <span>Turno: {tecnico.turno}</span>
-                            </div>
-                         
+            <h2>Técnicos:</h2>
+            {tecnicos.map((tecnico, index) => (
+                <div className="containerTecnico" key={index}>
+                    <div className="informacaoTecnico">
+                        <div className="tecnicoDados">
+                            Nome: {tecnico.nome}
                         </div>
+                        <div className="tecnicoDados">
+                            E-mail: {tecnico.email}
                         </div>
-                        
-                        
-                        <button className="excluir"  onClick={() => handleDeleteUser(tecnico.usuarioID)}>
-                            <span className="material-symbols-outlined">Delete</span>
-                        </button>
+                        <div className="tecnicoDados">
+                            <div className="tecnicoTipo">
+                                N{tecnico.tipoUsuario}
+                            </div>
+                            <div className="tecnicoTurno">
+                                <span>Turno: {tecnico.turno}</span>
+                            </div>
+
+                        </div>
                     </div>
-                ))}
-            </div>
+                    <button className="excluir" onClick={() => handleDeleteUser(tecnico.usuarioID)}>
+                        <span className="material-symbols-outlined">Delete</span>
+                    </button>
+                </div>
+            ))}
         </div>
     );
 };
- 
+
 export default TecnicosAdm;
- 
