0d06ce704ffd4bbe3944075d721dd501d4d36b0b
Filipe Colla <filipe10colla@gmail.com>
2024-05-04 15:04:26 -0300

    Feat: [ Back ] Função de escalamento de ticket para os técnicos N1, N2 e N3 feito.

--- front-end/src/components/EscalamentoTicket/index.tsx
+++ front-end/src/components/EscalamentoTicket/index.tsx
@@ -1,22 +1,42 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 import './escalamentoTicket.css';
+import ITickets from '../../types/ITickets';
 
-export const EscalamentoTicket = () => {
+interface Props {
+    selectedTicket: ITickets;
+}
 
+const EscalamentoTicket: React.FC<Props> = ({ selectedTicket }) => {
+    // eslint-disable-next-line no-empty-pattern
+    const [] = useState('');
+
+    const handleEscalamento = async (tecnico: string) => {
+        try {
+            const response = await axios.post('http://localhost:5555/tickets/alterarTecnico', {
+                ticketID: selectedTicket.ticketsID,
+                tipoTecnico: tecnico
+            });
+            console.log(response.data);
+            window.location.reload();
+
+        } catch (error) {
+            console.error('Erro ao escalonar o ticket:', error);
+        }
+    };
 
     return (
         <div>
             <div className="modalPerfil">
-
                 <h1>Escalar ticket</h1>
                 <div className="escalarTicket">
-                    <button className='btnEscalarTicket'>Técnico N1</button>
-                    <button  className='btnEscalarTicket'>Técnico N2</button>
-                    <button  className='btnEscalarTicket'>Técnico N3</button>
+                    <button className='btnEscalarTicket' onClick={() => handleEscalamento('1')}>Técnico N1</button>
+                    <button className='btnEscalarTicket' onClick={() => handleEscalamento('2')}>Técnico N2</button>
+                    <button className='btnEscalarTicket' onClick={() => handleEscalamento('3')}>Técnico N3</button>
                 </div>
-
-
             </div>
         </div>
-
     );
 };
+
+export default EscalamentoTicket;
